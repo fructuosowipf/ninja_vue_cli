0587178df381a4c4d365c303d3c61ae573c4931a
fructuosowipf <beide@fructuosowipf.com>
2020-05-15 14:57:13 +0200

    start lesson 21

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
-<template>
-  <div>
-    <h1>{{ title }}</h1>
-    <ninjas></ninjas>
-  </div>
-</template>
+<template></template>
 
 <script>
-// START Register component Ninja locally:
-import Ninjas from './Ninjas.vue';
-// END Register component Ninja locally:
-
 export default {
-  // START Register component Ninja locally:
-  components: {
-    'ninjas': Ninjas
-    },
-  // END Register component Ninja locally:
-  data () {
+  data() {
     return {
-      title: 'Ninja App'
-    }
+      
+    };
   }
-}
+};
 </script>
 
 <style scoped>
 h1 {
   color: palegreen;
 }
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-// START Register component Ninja globally:
 
-import Ninjas from "./Ninjas.vue"
-
-// Vue.component('ninjas', Ninjas)
-
-// END Register component Ninja globally:
 
 
 new Vue({
