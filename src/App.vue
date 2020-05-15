8ac708ea2bd5d1fb536b89145eaf1e08f2ff1eea
fructuosowipf <beide@fructuosowipf.com>
2020-05-15 15:19:33 +0200

    lesson 21 part

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
-<template></template>
+<template>
+<div>
+<app-header></app-header>
+<ninjas></ninjas>
+<app-footer></app-footer>
+</div>
+</template>
 
 <script>
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Ninjas from './components/Ninjas';
+
 export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'ninjas': Ninjas
+  },
   data() {
     return {
-      
+
     };
   }
 };
 </script>
 
 <style scoped>
-h1 {
-  color: palegreen;
-}
+
+
+
 </style>
---
+++ src/components/Ninjas.vue
@@ -0,0 +1,20 @@
+<template>
+<footer>
+    <p>{{ copyright }}</p>
+</footer>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      copyright: '© 2020, Rubén Fructuoso'
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
