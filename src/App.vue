0e58f3d5a1db6cac43f92898c9c0e2bcdc622d1d
fructuosowipf <beide@fructuosowipf.com>
2020-05-19 14:26:53 +0200

    lesson 27, slots, finished

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
-        <h1>I am the form helper</h1>
+        <form-helper>
+          <div slot="form-header">
+            <h3>This is the Title of the Form</h3>
+            <p>Information about the form</p>
+          </div>
+          <div slot="form-fields">
+            <input type="text" placeholder="name" required />
+            <input type="password" placeholder="password" required />
+          </div>
+          <div slot="form-controls">
+            <button @click="handleSubmit">Submit</button>
+          </div>
+        </form-helper>
     </div>
 </template>
 
@@ -14,7 +26,7 @@ export default {
     },
     data () {
         return {
-
+          title: 'I am a dynamic slot title'
         }
     },
     methods: {
