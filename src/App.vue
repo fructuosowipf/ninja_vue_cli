dcd6f17bafb7c5f1a661f5644ff66daa7c3aff97
fructuosowipf <beide@fructuosowipf.com>
2020-05-20 15:19:26 +0200

    start lesson 29

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,13 @@
 <template>
-  <div>
-    <button @click="component = 'form-one'">Show Form One</button>
-    <button @click="component = 'form-two'">Show Form Two</button>
-    <keep-alive>
-    <component v-bind:is="component"></component>
-    </keep-alive>
-  </div>
+  <div></div>
 </template>
 
 <script>
 // Imports
-import formOne from "./components/formOne";
-import formTwo from "./components/formTwo";
 
 export default {
-  components: {
-    "form-one": formOne,
-    "form-two": formTwo
-  },
   data() {
-    return {
-      component: false
-    };
+    return {};
   },
   methods: {}
 };
