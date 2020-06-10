6dc47164ae17016696a5f94ad006ee7f61c2c077
fructuosowipf <beide@fructuosowipf.com>
2020-06-10 13:37:04 +0200

    end lesson #41

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 // Imports
+import header from './components/header';
 import addBlog from './components/addBlog';
 import showBlogs from './components/showBlogs';
 import listBlogs from './components/listBlogs';
 
 export default {
   components: {
+    'app-header': header,
     'add-blog': addBlog,
     'show-blogs': showBlogs,
     'list-blogs': listBlogs
